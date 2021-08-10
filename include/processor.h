78f1d1404199e3a3c73b6b2292ae36669a8f5918
Brook Cheng <43509137+BrookPurdueUniversity@users.noreply.github.com>
2021-08-10 18:19:49 -0400

    Update processor.h

--- include/processor.h
+++ include/processor.h
@@ -1,6 +1,11 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <iostream>
+#include "linux_parser.h"
+#include <string>
+#include <vector>
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
