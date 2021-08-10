3caf761b4a70fb4eb832ae9eccca6e08cb9d6d4d
Brook Cheng <43509137+BrookPurdueUniversity@users.noreply.github.com>
2021-08-10 18:34:56 -0400

    Update process.h

--- include/process.h
+++ include/process.h
@@ -4,12 +4,14 @@
 #include "linux_parser.h"
 #include <string>
 
+using std::string;
 /*
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
 class Process {
  public:
+  Process(int pid);
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
@@ -21,13 +23,8 @@ class Process {
   // TODO: Declare any necessary private members
  private:
   int _pid{0};
-  string _usr{};
-  string _cmd{};
-  string _ram{};
-
-  float _seconds{0.0};
-  float _total_time{0.0};
-  float _cpu_util{0.0};
+  string _usr{}, _cmd{}, _ram{};
+  float _seconds{0.0}, _total_time{0.0}, _cpu_util{0.0};
 };
 
 #endif
